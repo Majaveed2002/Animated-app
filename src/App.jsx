a28348d225f4e1d2b4dbe9b3436fe7a9aa14a891
Ma Javeed <mohammedabduljaveed078@gmail.com>
2024-02-19 10:33:23 +0530

    created home layout with animations using gsap

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useLayoutEffect,useRef } from "react"
+import gsap from 'gsap'
+import Home from './components/Home'
+
+function App() {
+
+  const comp = useRef(null)
+
+  useLayoutEffect(()=>{
+    let ctx = gsap.context(()=>{
+      const t1 = gsap.timeline()
+      t1.from('#intro-slider',{
+        xPercent:'-100',
+        duration:1.3,
+        delay:0.3,
+      }).from(['#title-1','#title-2','#title-3'],{
+        opacity:0,
+        y:'+=30',
+        stagger:0.5,
+      }).to(['#title-1','#title-2','#title-3'],{
+        opacity:0,
+        y:'-=30',
+        delay:0.3,
+        stagger:0.5,
+      }).to('#intro-slider',{
+        xPercent:'-100',
+        duration:1.3
+      })
+    },comp)
+   
+    return () => ctx.revert()
+  },[])
+
+  return (
+    <div className="relative" ref={comp}>
+     <div id="intro-slider" className="h-screen p-10 bg-gray-50 absolute top-0 left-0 font-spaceGrotesk z-10 w-full flex flex-col gap-10 tracking-tight">
+      <h1 id="title-1" className="text-9xl">Software Engineer</h1>
+      <h1  id="title-2"  className="text-9xl">Frontend Developer</h1>
+      <h1  id="title-3"  className="text-9xl">Mern Stack Developer</h1>
+     </div>
+    {/* <div  className="h-screen flex bg-slate-200 justify-center items-center" >
+      <h1 id="welcome" className="text-9xl font-bold text-gray-100 font-spaceGrotesk">Welcome</h1>
+    </div> */}
+    <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,62 @@
+import React,{useRef} from 'react'
+import gsap from 'gsap'
+import { FaStar } from "react-icons/fa";
+import { IoLogoSnapchat } from "react-icons/io";
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
+
+
+export default function Home() {
+
+  const home = useRef(null)
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.fromTo('.home-left',{y:-150,delay:5,duration:1,ease:'bounce'},{y:0,duration:1,delay:5,ease:'bounce'})
+    .fromTo(".image",{x:150,duration:2,ease:'elastic'},{x:0,duration:1,ease:'elastic'})
+  },{scope:home})
+
+
+  return (
+    <div  ref={home} className='h-screen bg-white'>
+        <div className="w-full flex justify-evenly items-center mt-1 pt-3 pb-3">
+          <div className="logo">
+          <IoLogoSnapchat size={45} />
+          </div>
+          <ul className="flex justify-between items-center gap-6">
+
+            <li className="text-black font-spaceGrotesk list-none">Emotions</li>
+            <li className="text-black font-spaceGrotesk list-none">Manifesto</li>
+            <li className="text-black font-spaceGrotesk list-none">Self-awarness test</li>
+            <li className="text-black font-spaceGrotesk list-none">Work With Us</li>
+          </ul>
+          <div>
+          <button className='bg-black text-slate-100 p-2 rounded-lg text-center  pl-3 pr-3'>Download app</button>
+          </div>
+        </div>
+        <div className='flex justify-around bg-slate-200  h-[90%] w-[85%] rounded-3xl m-auto '>
+          <div  className='w-[40%] flex flex-col justify-center home-left'>
+            <h3 className='text-black text-lg font-spaceGrotesk'>Ahead app</h3>
+            <h1 className='text-black text-[45px] font-bold leading-tight font-spaceGrotesk'>Master your life <br/> by mastering <br/> emotions </h1>
+            <div className='flex justify-between items-center mt-5 w-[400px] gap-3'>
+              <img className='w-[150px]' src='https://res.cloudinary.com/daqedhqtx/image/upload/v1708272786/o1qufb2n2fyarppidmgl.png' alt='app-logo' />
+              <div className='text-black font-spaceGrotesk w-[300px]'>
+                <div className='flex gap-1'>
+                <FaStar size={25} color='gold' />
+                <FaStar size={25} color='gold' />
+                <FaStar size={25} color='gold' />
+                <FaStar size={25} color='gold' />
+                <FaStar size={25} color='gold' />
+                </div>
+                100+ AppStore reviews
+              </div>
+            </div>
+          </div>
+          <div className='w-[40%]  flex justify-center items-center'>
+            <img  src="https://res.cloudinary.com/daqedhqtx/image/upload/f_auto,q_auto/bl3jsilqbcnmn9lapwxx" alt="phone-img" className='w-[350px] image' />
+          </div>
+        </div>
+    </div>
+  )
+}
